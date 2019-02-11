c20a98f2afa051d464b935c6f374c8deb6b189c8
Giovanni Russo Boscoli <boscoli.giovanni@gmail.com>
2019-02-11 21:57:02 +0000

    Changed Visual Studio Version

--- StylesSamples/wwwroot/lib/js/CustomSamples.js
+++ StylesSamples/wwwroot/lib/js/CustomSamples.js
@@ -209,62 +209,38 @@ function ReadDevicesFromJsonFile(_showByArea) {
 
 
 function createDisplayDevicesShowByArea(devices, checkStateDevicesCallback) {
-
-
-
-    var qttDevicesPerRow;
-
-    var deviceLength;
-
+    
+    var qttDevicesPerRow = 6;
+    var deviceLength = 0;
     var _html;
-
-    var rows;
-
+    var rows = 0;
     var countDevices;
+    var colSize = 12 / qttDevicesPerRow; //(12 columns from Bootstrap)
 
     $("#divDevices").html("");
 
     $.each(devices, function (i, item) {
 
-
-
-        qttDevicesPerRow = 6;
-
-        deviceLength = 0;
-
         _html = "";
-
-        rows = 0;
-
-        countDevices = 0;
-
-
-
+        countDevices = 0;        
+                
         _html += "<div class='row row'" + item.area + " id='row_" + i + "'>";
 
         _html += "<fieldset>";
 
         _html += "<legend>" + item.area + "</legend>";
-
-
-
+        
         deviceLength = $(item.devices).length;
 
         rows = Math.floor(deviceLength / qttDevicesPerRow) + (deviceLength % qttDevicesPerRow > 0 ? 1 : 0);//define how many rows will be necessary to create based in 12 columns[bootstrap]
-
-
-
+        
         for (i = 0; i < rows; i++) {
-
-
 
             _html += "<div class='row rowBtnControlDevicesByArea' id='rowBtnControlDevicesByArea_" + i + "'>";
 
             _html += "Show control buttons by area...";
 
             _html += "</div>";
-
-
 
             _html += "<div class='row rowClassItem' id='row_" + i + "'>";
 
@@ -276,7 +252,7 @@ function createDisplayDevicesShowByArea(devices, checkStateDevicesCallback) {
 
                 }
 
-                _html += "<div class='col-lg-2 itemDevice' id='row_" + i + "_col_" + j + "'>";
+                _html += "<div class='col-sm-2 col-md-2 col-lg-2 itemDevice' id='row_" + i + "_col_" + j + "'>";
 
                 _html += "<div id='divImgRow_" + i + "_col_" + j + "'>";
 
@@ -304,8 +280,6 @@ function createDisplayDevicesShowByArea(devices, checkStateDevicesCallback) {
 
         }
 
-
-
         _html += "</fieldset>";
 
         _html += "</div> ";
@@ -317,7 +291,6 @@ function createDisplayDevicesShowByArea(devices, checkStateDevicesCallback) {
     registerClickDevice();
 
     checkStateDevicesCallback();
-
 }
 
 
@@ -325,14 +298,11 @@ function createDisplayDevicesShowByArea(devices, checkStateDevicesCallback) {
 function createDisplayDevices(devices, checkStateDevicesCallback) {
 
     var qttDevicesPerRow = 6;
-
     var deviceLength = $(devices).length;
-
     var _html = "";
-
     var rows = Math.floor(deviceLength / qttDevicesPerRow) + (deviceLength % qttDevicesPerRow > 0 ? 1 : 0);//define how many rows will be necessary to create based in 12 columns[bootstrap]
-
     var countDevices = 0;
+    var colSize = 12 / qttDevicesPerRow; //(12 columns from Bootstrap)
 
     $("#divDevices").html("");
 
@@ -343,12 +313,10 @@ function createDisplayDevices(devices, checkStateDevicesCallback) {
         for (j = 0; j < qttDevicesPerRow; j++) {
 
             if (countDevices >= deviceLength) {
-
                 break;
-
             }
 
-            _html += "<div class='col-lg-2 itemDevice' id='row_" + i + "_col_" + j + "'>";
+            _html += "<div class='col-sm-2 col-md-2 col-lg-2 itemDevice' id='row_" + i + "_col_" + j + "'>";
 
             _html += "<div id='divImgRow_" + i + "_col_" + j + "'>";
 
@@ -373,19 +341,11 @@ function createDisplayDevices(devices, checkStateDevicesCallback) {
         }
 
         _html += "</div> <hr />";
-
-        //_html += "</div> ";
-
     }
 
-
-
     $("#divDevices").html(_html);
-
     registerClickDevice();
-
     checkStateDevicesCallback();
-
 }
 
 
